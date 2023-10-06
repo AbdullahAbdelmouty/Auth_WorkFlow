26ad2144aac291bc9a3a7ea46b54e63ac41c098a
Abdullah <abdallah0115019@gmail.com>
2023-10-06 14:51:13 +0300

    update

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use('/api/v1/users',proxy,userRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const PORT = 5000||process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const start = async()=>{
     try {
